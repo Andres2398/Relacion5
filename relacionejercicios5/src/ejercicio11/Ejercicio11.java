23978baf41ea33784182d401242755e3b8a695fc
1DAM-11 <1DAM-11@1ASIR-11>
2024-11-08 20:55:52 +0100

    dfsa

--- relacionejercicios5/src/ejercicio11/Ejercicio11.java
+++ relacionejercicios5/src/ejercicio11/Ejercicio11.java
@@ -1,17 +1,44 @@
 package ejercicio11;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio11 {
 	
 	public static void main(String[] args) {
+		//Averiguar si el valor de un entero, se encuentra o no en dos arrays indicando la 
+		//posición o posiciones. 
 		
+		int[] v = new int [20];
+		int[] x = new int [20];
+		Random r= new Random();
+		int random;
 		
+		for (int i = 0; i < x.length; i++) {
+			random=r.nextInt();
+			v[i]=random;
+			x[i]=random;
+			
+		}
 		
-		
-		
-		
-		
-		
-		
+		System.out.println("dame un valor");
+		Scanner sc = new Scanner(System.in);
+		int numero=sc.nextInt();
+		posicion x=
+		for (int i = 0; i < v.length; i++) {
+			if (v[i]==numero) {
+				
+			}
+			
+				
+			for (int j = 0; j < x.length; j++) {
+				if (x[i]==numero) {
+						
+					}
+				}
+				
+			}
+		}
 		
 		
 		
